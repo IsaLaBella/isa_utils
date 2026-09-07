279580269e60ac217ff89a1c8e9a3a092317353c
Isabelle Chamberlain <isabelle.str.cham@gmail.com>
2026-09-07 20:27:02 +0200

    Add support for color representation with 8 bits per channel.
    Underlying color is still represented with r, g and b as a float from 0 to 1.

--- isa_utils/color/__init__.py
+++ isa_utils/color/__init__.py
@@ -3,16 +3,54 @@ Submodule that implements a basic color class.
 """
 
 # ---------------------------------------------------------------------------- #
+#                                    Imports                                   #
+# ---------------------------------------------------------------------------- #
+
+from typing import Any, Tuple, TypeAlias
+
+# ---------------------------------------------------------------------------- #
+#                               Helper Functions                               #
+# ---------------------------------------------------------------------------- #
+
+def _clamp(value: float | int, min_value: float | int, max_value: float | int) -> float | int:
+    return min(max_value, max(min_value, value))
+
+def _float_to_8_bit_unsigned(value: float, min_float_value: float = 0, max_float_value: float = 1) -> int:
+    new_value = value*(255-0)/(max_float_value - min_float_value)
+    return int(_clamp(new_value, 0, 255))
+
+def _8_bit_unsigned_to_float(value: int, min_float_value: float = 0, max_float_value: float = 1) -> float:
+    value = _clamp(value, 0, 255)
+    return value*(max_float_value - min_float_value)/(255-0)
+     
+# ---------------------------------------------------------------------------- #
 #                                    Classes                                   #
 # ---------------------------------------------------------------------------- #
 
+Color_Mode: TypeAlias = int
+class COLOR_MODES():
+
+    _MIN = 0 # Minumum value for error checking - do not use
+    FLOAT_0_TO_1 = 1
+    UINT8 = 2
+    _MAX = 3 # Maximum value for error checking - do not use
+
 class Color():
+
+    __MAX_FLOAT_VAL = 1
+    __MIN_FLOAT_VAL = 0
 
     def __init__(self, *args, **kwargs):
 
+        # Defaults
+        self.__r = 0
+        self.__g = 0
+        self.__b = 0
+
+        self.color_mode = kwargs.get("color_mode", COLOR_MODES.FLOAT_0_TO_1)
+
         match len(args):
             case 0:
-                # Defaults
                 self.r = self.g = self.b = 0
             case 1:
                 self.r = self.g = self.b = args[0]
@@ -27,28 +65,157 @@ class Color():
         self.g = kwargs.get("g", self.g)
         self.b = kwargs.get("b", self.b)
 
-        
+        # self.r_8bit = kwargs.get("r_8bit", self.r_8bit)
+        # self.g_8bit = kwargs.get("g_8bit", self.g_8bit)
+        # self.b_8bit = kwargs.get("b_8bit", self.b_8bit)
 
-    # -------------------------- Properties: r, g and b -------------------------- #
+    # -------------------------- Properties: Color Mode -------------------------- #
+
+    def __get_color_mode(self) -> Color_Mode:
+        return self.__color_mode
+
+    def __set_color_mode(self, color_mode: Color_Mode) -> None:
+
+        if not isinstance(color_mode, Color_Mode):
+            raise TypeError(f"Invalid type for color mode: {color_mode.__class__.__name__}")
+        
+        if color_mode <= COLOR_MODES._MIN or color_mode >= COLOR_MODES._MAX:
+            raise ValueError(f"Invalid value for color mode: {color_mode}.")
+        
+        self.__color_mode = color_mode
+
+    color_mode = property(__get_color_mode, __set_color_mode)
+
+    # --------------------- Properties: r, g and b as floats --------------------- #
     
-    def __get_r(self):
+    def __get_r_float(self) -> float:
         return self.__r
 
-    def __set_r(self, value: float):
-        self.__r = min(max(value, 0), 1)
+    def __set_r_float(self, value: float):
+        self.__r = _clamp(value, self.__MIN_FLOAT_VAL, self.__MAX_FLOAT_VAL)
 
-    def __get_g(self):
+    def __get_g_float(self) -> float:
             return self.__g
     
-    def __set_g(self, value: float):
-        self.__g = min(max(value, 0), 1)
+    def __set_g_float(self, value: float):
+        self.__g = _clamp(value, self.__MIN_FLOAT_VAL, self.__MAX_FLOAT_VAL)
 
-    def __get_b(self):
+    def __get_b_float(self) -> float:
             return self.__b
     
-    def __set_b(self, value: float):
-        self.__b = min(max(value, 0), 1)
+    def __set_b_float(self, value: float):
+        self.__b = _clamp(value, self.__MIN_FLOAT_VAL, self.__MAX_FLOAT_VAL)
+
+    r_float = property(__get_r_float, __set_r_float)
+    g_float = property(__get_g_float, __set_g_float)
+    b_float = property(__get_b_float, __set_b_float)
+
+    @property
+    def rgb_float(self) -> Tuple[float, float, float]:
+        return (self.r, self.g, self.b)
+
+    # -------------- Properties: r, g and b as 8-bit unsigned values ------------- #
+    
+    def __get_r_8bit(self) -> int:
+        return _float_to_8_bit_unsigned(self.__r)
+    
+    def __set_r_8bit(self, value: int):
+        self.__r = _8_bit_unsigned_to_float(value, self.__MIN_FLOAT_VAL, self.__MAX_FLOAT_VAL)
+
+    def __get_g_8bit(self) -> int:
+        return _float_to_8_bit_unsigned(self.__g)
+    
+    def __set_g_8bit(self, value: int):
+        self.__g = _8_bit_unsigned_to_float(value, self.__MIN_FLOAT_VAL, self.__MAX_FLOAT_VAL)
+
+    def __get_b_8bit(self) -> int:
+        return _float_to_8_bit_unsigned(self.__b)
+    
+    def __set_b_8bit(self, value: int):
+        self.__b = _8_bit_unsigned_to_float(value, self.__MIN_FLOAT_VAL, self.__MAX_FLOAT_VAL)
+
+    r_8bit = property(__get_r_8bit, __set_r_8bit)
+    g_8bit = property(__get_g_8bit, __set_g_8bit)
+    b_8bit = property(__get_b_8bit, __set_b_8bit)
+
+    @property
+    def rgb_24bit(self) -> Tuple[int, int, int]:
+        return (self.r_8bit, self.g_8bit, self.b_8bit)
+
+    # ---------------- Properties: r, g and b based on color mode ---------------- #
+
+    def __get_r(self) -> float | int:
+
+        match self.color_mode:
+            case COLOR_MODES.FLOAT_0_TO_1:
+                return self.r_float
+            case COLOR_MODES.UINT8:
+                return self.r_8bit
+            case _:
+                raise ValueError(f"{self.__class__.__name__} has invalid value for color_mode ({self.color_mode}).")
+
+    def __set_r(self, value: float | int):
+        
+        match self.color_mode:
+            case COLOR_MODES.FLOAT_0_TO_1:
+                self.r_float = value
+            case COLOR_MODES.UINT8:
+                self.r_8bit = value
+            case _:
+                raise ValueError(f"{self.__class__.__name__} has invalid value for color_mode ({self.color_mode}).")
+
+    def __get_g(self) -> float:
+
+        match self.color_mode:
+            case COLOR_MODES.FLOAT_0_TO_1:
+                return self.g_float
+            case COLOR_MODES.UINT8:
+                return self.g_8bit
+            case _:
+                raise ValueError(f"{self.__class__.__name__} has invalid value for color_mode ({self.color_mode}).")
+    
+    def __set_g(self, value: float | int):
+        
+        match self.color_mode:
+            case COLOR_MODES.FLOAT_0_TO_1:
+                self.g_float = value
+            case COLOR_MODES.UINT8:
+                self.g_8bit = value
+            case _:
+                raise ValueError(f"{self.__class__.__name__} has invalid value for color_mode ({self.color_mode}).")
+
+    def __get_b(self) -> float:
+
+        match self.color_mode:
+            case COLOR_MODES.FLOAT_0_TO_1:
+                return self.b_float
+            case COLOR_MODES.UINT8:
+                return self.b_8bit
+            case _:
+                raise ValueError(f"{self.__class__.__name__} has invalid value for color_mode ({self.color_mode}).")
+    
+    def __set_b(self, value: float | int):
+        
+        match self.color_mode:
+            case COLOR_MODES.FLOAT_0_TO_1:
+                self.b_float = value
+            case COLOR_MODES.UINT8:
+                self.b_8bit = value
+            case _:
+                raise ValueError(f"{self.__class__.__name__} has invalid value for color_mode ({self.color_mode}).")
 
     r = property(__get_r, __set_r)
     g = property(__get_g, __set_g)
     b = property(__get_b, __set_b)
+
+    @property
+    def rgb(self) -> Tuple[float | int, float | int, float | int]:
+        return (self.r, self.g, self.b)
+
+    # ----------------------------------- Other ---------------------------------- #
+
+    def __repr__(self) -> str:
+        return f"<Color object: {self.r}, {self.g}, {self.b}>"
+
+    def __str__(self) -> str:
+        return f"(r={self.r}, g={self.g}, b={self.b})"
