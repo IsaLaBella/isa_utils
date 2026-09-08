9a2fab94a91c3f0134e6c3f188dd0d5f16610b87
Isabelle Chamberlain <isabelle.chamberlain@consultant.volvo.com>
2026-09-08 15:53:29 +0200

    Change argument validation of assignments to rgb color channels.
    Type validation for assignments to rgb channels is performed by explicit cast (e.g. value = float(value)).
    
    Assignments of invalid values to rgb channels (e.g. trying to set Color.r_float to 1.1) now raises a ValueError instead of silently clamping the value to 1.0. This avoids bugs due to situations where the user tries to set a color channel to a uint8 value while the color object is in float mode (e.g. Color.r = 255 would silently cause Color.__r to be 1.0 if Color.color_mode is FLOAT_0_TO_1, but now it instead raises an exception).
    
    Created class methods for argument validation and exception raising, as well as conversion between representations.
    
    Removed external helper functions as they are now unused.

--- isa_utils/color/__init__.py
+++ isa_utils/color/__init__.py
@@ -8,21 +8,6 @@ Submodule that implements a basic color class.
 
 from typing import Any, Tuple, TypeAlias
 from enum import Enum, auto
-
-# ---------------------------------------------------------------------------- #
-#                               Helper Functions                               #
-# ---------------------------------------------------------------------------- #
-
-def _clamp(value: float | int, min_value: float | int, max_value: float | int) -> float | int:
-    return min(max_value, max(min_value, value))
-
-def _float_to_8_bit_unsigned(value: float, min_float_value: float = 0, max_float_value: float = 1) -> int:
-    new_value = value*(255-0)/(max_float_value - min_float_value)
-    return int(_clamp(new_value, 0, 255))
-
-def _8_bit_unsigned_to_float(value: int, min_float_value: float = 0, max_float_value: float = 1) -> float:
-    value = _clamp(value, 0, 255)
-    return value*(max_float_value - min_float_value)/(255-0)
      
 # ---------------------------------------------------------------------------- #
 #                                    Classes                                   #
@@ -35,15 +20,18 @@ class COLOR_MODE(Enum):
 
 class Color():
 
-    __MAX_FLOAT_VAL = 1
-    __MIN_FLOAT_VAL = 0
+    __MAX_RGB_FLOAT_VAL = 1
+    __MIN_RGB_FLOAT_VAL = 0
+
+    __MIN_RGB_UINT8_VAL = 0
+    __MAX_RGB_UINT8_VAL = 255
 
     def __init__(self, *args, **kwargs):
 
         # Defaults
-        self.__r = 0
-        self.__g = 0
-        self.__b = 0
+        self.__r = 0.0
+        self.__g = 0.0
+        self.__b = 0.0
 
         self.color_mode = kwargs.get("color_mode", COLOR_MODE.FLOAT_0_TO_1)
 
@@ -80,24 +68,36 @@ class Color():
     color_mode = property(__get_color_mode, __set_color_mode)
 
     # --------------------- Properties: r, g and b as floats --------------------- #
+
+    @classmethod
+    def __check_rgb_float_value(cls, value: float) -> float:
+        try:
+            value = float(value) # Converting to float before comparison raises more sensible type errors for invalid types (i.e. 'could not convert x to float' instead of 'cannot compare float to x')
+        except ValueError:
+            raise TypeError(f"Invalid type for float value of rgb channel. Must be float or convertible to float.")
+        
+        if value > cls.__MAX_RGB_FLOAT_VAL or value < cls.__MIN_RGB_FLOAT_VAL:
+            raise ValueError(f"Invalid float value for rgb channel of {cls.__name__}. Must be between {cls.__MIN_RGB_FLOAT_VAL} and {cls.__MAX_RGB_FLOAT_VAL}.")
+
+        return value
     
     def __get_r_float(self) -> float:
         return self.__r
 
     def __set_r_float(self, value: float):
-        self.__r = _clamp(value, self.__MIN_FLOAT_VAL, self.__MAX_FLOAT_VAL)
+        self.__r = self.__check_rgb_float_value(value)
 
     def __get_g_float(self) -> float:
-            return self.__g
+        return self.__g
     
     def __set_g_float(self, value: float):
-        self.__g = _clamp(value, self.__MIN_FLOAT_VAL, self.__MAX_FLOAT_VAL)
+        self.__g = self.__check_rgb_float_value(value)
 
     def __get_b_float(self) -> float:
-            return self.__b
+        return self.__b
     
     def __set_b_float(self, value: float):
-        self.__b = _clamp(value, self.__MIN_FLOAT_VAL, self.__MAX_FLOAT_VAL)
+        self.__b = self.__check_rgb_float_value(value)
 
     r_float = property(__get_r_float, __set_r_float)
     g_float = property(__get_g_float, __set_g_float)
@@ -108,24 +108,47 @@ class Color():
         return (self.r_float, self.g_float, self.b_float)
 
     # -------------- Properties: r, g and b as 8-bit unsigned values ------------- #
+
+    @classmethod
+    def __check_rgb_uint8_value(cls, value: int) -> int:
+        try:
+            value = int(value) # Converting to int before comparison raises more sensible type errors for invalid types (i.e. 'could not convert x to int' instead of 'cannot compare int to x')
+        except ValueError:
+            raise TypeError(f"Invalid type for uint8 value of rgb channel. Must be int or convertible to int.")
+        
+        if value > cls.__MAX_RGB_UINT8_VAL or value < cls.__MIN_RGB_UINT8_VAL:
+            raise ValueError(f"Invalid uint8 value for rgb channel of {cls.__name__}. Must be between {cls.__MIN_RGB_FLOAT_VAL} and {cls.__MAX_RGB_FLOAT_VAL}.")
+
+        return value
+
+    @classmethod
+    def __rgb_uint8_to_float(cls, value: int) -> float:
+        return float(value*(cls.__MAX_RGB_FLOAT_VAL-cls.__MIN_RGB_FLOAT_VAL)/(cls.__MAX_RGB_UINT8_VAL-cls.__MIN_RGB_UINT8_VAL))
+
+    @classmethod
+    def __rgb_float_to_uint8(cls, value: float) -> int:
+        return int(round(value*(cls.__MAX_RGB_UINT8_VAL-cls.__MIN_RGB_UINT8_VAL)/(cls.__MAX_RGB_FLOAT_VAL-cls.__MIN_RGB_FLOAT_VAL)))
     
     def __get_r_8bit(self) -> int:
-        return _float_to_8_bit_unsigned(self.__r)
+        return self.__rgb_float_to_uint8(self.__r)
     
     def __set_r_8bit(self, value: int):
-        self.__r = _8_bit_unsigned_to_float(value, self.__MIN_FLOAT_VAL, self.__MAX_FLOAT_VAL)
+        value = self.__check_rgb_uint8_value(value)
+        self.__r = self.__rgb_uint8_to_float(value)
 
     def __get_g_8bit(self) -> int:
-        return _float_to_8_bit_unsigned(self.__g)
+        return self.__rgb_float_to_uint8(self.__g)
     
     def __set_g_8bit(self, value: int):
-        self.__g = _8_bit_unsigned_to_float(value, self.__MIN_FLOAT_VAL, self.__MAX_FLOAT_VAL)
+        value = self.__check_rgb_uint8_value(value)
+        self.__g = self.__rgb_uint8_to_float(value)
 
     def __get_b_8bit(self) -> int:
-        return _float_to_8_bit_unsigned(self.__b)
+        return self.__rgb_float_to_uint8(self.__b)
     
     def __set_b_8bit(self, value: int):
-        self.__b = _8_bit_unsigned_to_float(value, self.__MIN_FLOAT_VAL, self.__MAX_FLOAT_VAL)
+        value = self.__check_rgb_uint8_value(value)
+        self.__b = self.__rgb_uint8_to_float(value)
 
     r_8bit = property(__get_r_8bit, __set_r_8bit)
     g_8bit = property(__get_g_8bit, __set_g_8bit)
@@ -145,7 +168,7 @@ class Color():
             case COLOR_MODE.UINT8:
                 return self.r_8bit
             case _:
-                raise ValueError(f"{self.__class__.__name__} has invalid or not implemented value for color_mode ({self.color_mode}).")
+                raise ValueError(f"{self.__class__.__name__} {self.__name__} has invalid or not implemented value for color_mode ({self.color_mode}).")
 
     def __set_r(self, value: float | int):
         
@@ -155,7 +178,7 @@ class Color():
             case COLOR_MODE.UINT8:
                 self.r_8bit = value
             case _:
-                raise ValueError(f"{self.__class__.__name__} has invalid or not implemented value for color_mode ({self.color_mode}).")
+                raise ValueError(f"{self.__class__.__name__} {self.__name__} has invalid or not implemented value for color_mode ({self.color_mode}).")
 
     def __get_g(self) -> float:
 
@@ -165,7 +188,7 @@ class Color():
             case COLOR_MODE.UINT8:
                 return self.g_8bit
             case _:
-                raise ValueError(f"{self.__class__.__name__} has invalid or not implemented value for color_mode ({self.color_mode}).")
+                raise ValueError(f"{self.__class__.__name__} {self.__name__} has invalid or not implemented value for color_mode ({self.color_mode}).")
     
     def __set_g(self, value: float | int):
         
@@ -175,7 +198,7 @@ class Color():
             case COLOR_MODE.UINT8:
                 self.g_8bit = value
             case _:
-                raise ValueError(f"{self.__class__.__name__} has invalid or not implemented value for color_mode ({self.color_mode}).")
+                raise ValueError(f"{self.__class__.__name__} {self.__name__} has invalid or not implemented value for color_mode ({self.color_mode}).")
 
     def __get_b(self) -> float:
 
@@ -185,7 +208,7 @@ class Color():
             case COLOR_MODE.UINT8:
                 return self.b_8bit
             case _:
-                raise ValueError(f"{self.__class__.__name__} has invalid or not implemented value for color_mode ({self.color_mode}).")
+                raise ValueError(f"{self.__class__.__name__} {self.__name__} has invalid or not implemented value for color_mode ({self.color_mode}).")
     
     def __set_b(self, value: float | int):
         
@@ -195,7 +218,7 @@ class Color():
             case COLOR_MODE.UINT8:
                 self.b_8bit = value
             case _:
-                raise ValueError(f"{self.__class__.__name__} has invalid or not implemented value for color_mode ({self.color_mode}).")
+                raise ValueError(f"{self.__class__.__name__} {self.__name__} has invalid or not implemented value for color_mode ({self.color_mode}).")
 
     r = property(__get_r, __set_r)
     g = property(__get_g, __set_g)
@@ -211,4 +234,4 @@ class Color():
         return f"<Color object: {self.r}, {self.g}, {self.b}>"
 
     def __str__(self) -> str:
-        return f"(r={self.r}, g={self.g}, b={self.b})"
+        return f"Color(r={self.r}, g={self.g}, b={self.b})"
